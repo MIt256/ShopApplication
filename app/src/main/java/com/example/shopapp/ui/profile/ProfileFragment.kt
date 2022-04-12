5aa6b2d698925e8d0b1bf0ff5e1d68759508ba5e
Maks <xxzanko@gmail.com>
2022-04-12 16:50:36 +0300

    fix map

--- app/src/main/java/com/example/shopapp/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/shopapp/ui/profile/ProfileFragment.kt
@@ -1,21 +1,29 @@
 package com.example.shopapp.ui.profile
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat.requestPermissions
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.shopapp.databinding.FragmentProfileBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
 
-class ProfileFragment : Fragment() {
+class ProfileFragment : Fragment(), OnMapReadyCallback{
 
     private var _binding: FragmentProfileBinding? = null
-    private lateinit var mMapView: MapView
-    private lateinit var googleMap: GoogleMap
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private lateinit var nmap: GoogleMap
+    private lateinit var mapView: MapView
+    private lateinit var lastLocation: Location
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -31,29 +39,50 @@ class ProfileFragment : Fragment() {
 
         profileViewModel.text.observe(viewLifecycleOwner) {
             //binding.textProfile.text = it
+            //Todo get data
         }
 
-        mMapView = binding.mapView
-        mMapView.onCreate(savedInstanceState)
-        mMapView.onResume()
-        try {
-            MapsInitializer.initialize(requireActivity().applicationContext)
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        mMapView.getMapAsync(OnMapReadyCallback { mMap ->
-            googleMap = mMap
-
-
-
-        })
+        mapView = binding.mapView
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+        fusedLocationClient =LocationServices.getFusedLocationProviderClient(requireActivity())
 
         return root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onMapReady(googleMap: GoogleMap) {
+        nmap = googleMap
+        nmap.setMinZoomPreference(12F)
+        setUpMap()
+    }
+
+    private fun setUpMap() {
+        if( (ContextCompat.checkSelfPermission(requireContext(),Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED)
+        ) {
+            requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),101)
+        }
+            nmap.isMyLocationEnabled = true
+            activity?.let { location ->
+                fusedLocationClient.lastLocation.addOnSuccessListener(location) {
+                    if (it != null) {
+                        lastLocation = it
+                        var currentLatLong = LatLng(it.latitude, it.longitude)
+                        nmap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLong, 14f))
+                    }
+                }
+            }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 
 
